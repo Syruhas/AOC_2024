04ba200c1cc3651c45e1b11372e257c51aa67174
Leo Dadi <leo.dadi@student-cs.fr>
2024-12-01 19:20:28 +0100

    day 1 part two

--- 1/1.py
+++ 1/1.py
@@ -8,7 +8,7 @@ def parse_file(path:str)-> tuple[list, list]:
             list2.append(int(line_list[1]))
     return list1, list2
 
-def compute_distance(list1, list2):
+def compute_distance(list1:list[int], list2:list[int]) -> int:
     list1.sort()
     list2.sort()
     total_distance = 0
@@ -22,10 +22,37 @@ def compute_distance(list1, list2):
     return total_distance
 
 
-if __name__=="__main__":
+def compute_similarity_score(list1:list[int], list2:list[int]) -> int:
+    number_of_occurence = {}
+    similarity_score = 0
+
+    for value in list1:
+        if(number_of_occurence.get(value)):
+            break
+        number_of_occurence[value] = list2.count(value)
+
+    for value in list1:
+        similarity_score += value * number_of_occurence.get(value, 0)
+
+    return similarity_score
+
+
+def part_one():
     list1, list2 = parse_file("input.txt")
-    #print(list1)
-    #print(list2)
+    print(list1)
+    print(list2)
 
     total_dist = compute_distance(list1, list2)
     print(total_dist)
+
+def part_two():
+    list1, list2 = parse_file("input.txt")
+    print(list1)
+    print(list2)
+
+    sim_score = compute_similarity_score(list1, list2)
+    print(sim_score)
+
+
+if __name__=="__main__":
+    part_two()
